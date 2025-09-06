b9c6f01ef8987f32557a7c6dec2be1bf7e9cc3b2
Krishna Bhat <memotoskbhat@gmail.com>
2025-09-06 22:13:43 +0530

    Updated config to date

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,94 @@
+return {
+  --   {
+  --     "rose-pine/neovim",
+  --     name = "rose-pine",
+  --     lazy = true,
+  --     config = function()
+  --       local config = require("rose-pine")
+  --
+  --       config.setup({
+  --         variant = "moon", -- auto, main, moon, or dawn
+  --         dark_variant = "moon", -- main, moon, or dawn
+  --         dim_inactive_windows = false,
+  --         extend_background_behind_borders = true,
+  --         disable_background = true,
+  --
+  --         styles = {
+  --           bold = true,
+  --           italic = false,
+  --           transparency = false,
+  --         },
+  --
+  --         groups = {
+  --           border = "muted",
+  --           link = "iris",
+  --           panel = "surface",
+  --
+  --           error = "love",
+  --           hint = "iris",
+  --           info = "foam",
+  --           warn = "gold",
+  --
+  --           git_add = "foam",
+  --           git_change = "rose",
+  --           git_delete = "love",
+  --           git_dirty = "rose",
+  --           git_ignore = "muted",
+  --           git_merge = "iris",
+  --           git_rename = "pine",
+  --           git_stage = "iris",
+  --           git_text = "rose",
+  --           git_untracked = "subtle",
+  --
+  --           headings = {
+  --             h1 = "iris",
+  --             h2 = "foam",
+  --             h3 = "rose",
+  --             h4 = "gold",
+  --             h5 = "pine",
+  --             h6 = "foam",
+  --           },
+  --           -- Alternatively, set all headings at once.
+  --           -- headings = "subtle",
+  --         },
+  --
+  --         highlight_groups = {
+  --           -- Comment = { fg = "foam" },
+  --           -- VertSplit = { fg = "muted", bg = "muted" },
+  --         },
+  --
+  --         before_highlight = function(group, highlight, palette)
+  --           -- Disable all undercurls
+  --           -- if highlight.undercurl then
+  --           --     highlight.undercurl = false
+  --           -- end
+  --           --
+  --           -- Change palette colour
+  --           -- if highlight.fg == palette.pine then
+  --           --     highlight.fg = palette.foam
+  --           -- end
+  --         end,
+  --       })
+  --
+  --       vim.cmd("colorscheme rose-pine")
+  --     end,
+  --   },
+  {
+    "projekt0n/github-nvim-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("github-theme").setup({
+        -- ...
+      })
+
+      vim.cmd("colorscheme github_dark_default")
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "github_dark_default",
+    },
+  },
+}
