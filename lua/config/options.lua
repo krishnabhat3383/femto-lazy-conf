fb678728bd964f0871d34dc53fcb9d0b786dd848
Krishna Bhat <memotoskbhat@gmail.com>
2025-09-17 18:08:42 +0530

    Updated options for python to use basedpyright instead of default

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- originally it was the following:
+-- vim.g.lazyvim_python_lsp = "pyright"
+-- now changed to:
+vim.g.lazyvim_python_lsp = "basedpyright"
